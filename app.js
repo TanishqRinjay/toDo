3c3e5a02dfa96d92f2ad350f8e63d46110a6d352
TanishqRinjay <tanishqbaranwal@gmail.com>
2023-02-03 18:08:32 +0530

    Added funtionality to add Task that is previously stored in local Storage

--- app.js
+++ app.js
@@ -1,13 +1,62 @@
-let addButton = document.getElementById("add");
-let addedTasks = document.querySelector(".addedTask");
-let title = document.getElementById("title");
-let description = document.getElementById("description");
-let noOfTasks=0;
-var taskNo=0;
-if(taskNo<=0)
+let addButton = document.getElementById("add");             //Add button to add Task
+let addedTasks = document.querySelector(".addedTask");      //Parent div to append task blocks
+let title = document.getElementById("title");               //Title of Task that is added
+let description = document.getElementById("description");   //Description of Task that is added
+var taskNo=0;                                               //To count no. of task that is added
+
+function fillFromLocal(){       //Function to fill-in previously added task on local Storage
+    if(localStorage.length!=0){
+        for(let i=1; i<=localStorage.length; i++){
+            
+            //Converting Local Storage String to Array
+            
+            let lArray = JSON.parse(localStorage.getItem(`toDo-${i}`));
+            
+            //Adding previously added tasks that was saved on LocalStorage
+
+            addedTasks.innerHTML+=`<div class="tasks">
+            <div class="content">
+            <h2>${lArray[0]}</h2>
+            <p>${lArray[1]}</p>
+            </div>
+            <div class="button">
+            <button class="delete"><i class="fa-solid fa-trash"></i></button>
+            </div>
+            </div>`
+            taskNo++;   //  To increase task no. at start
+
+            //Adding delete button on task entered by LocalStorage
+            var deleteBtn = document.querySelectorAll(".delete");
+            for(let i=0; i<deleteBtn.length; i++){
+                deleteBtn[i].onclick = function(){
+                    this.parentNode.parentNode.remove();
+                    localStorage.removeItem(`toDo-${taskNo}`, JSON.stringify([title.value, description.value]))
+                    taskNo--;
+                    console.log(taskNo);
+                    if(taskNo==0)
+                    {
+                        showEmpty();
+                    }
+                }
+            }
+        }
+        console.log(localStorage.getItem(`toDo-1`))
+    }
+}
+fillFromLocal();        //Calling funtion to fill-in task from LocalStorage
+
+
+function showEmpty(){       //Funtion to show text when no task is there
+    addedTasks.innerHTML = `<h3 class="noTask">Entered tasks will be shown here</h3>`;
+}
+
+
+if(taskNo<=0)       //To show "Added task will be shown here" line
 {
     showEmpty();
 }
+
+//      Add button funtionality added
 addButton.addEventListener('click', (e)=>{
     console.log(e);
     taskNo++;
@@ -43,6 +92,3 @@ addButton.addEventListener('click', (e)=>{
         }
     }
 })
-function showEmpty(){
-    addedTasks.innerHTML = `<h3 class="noTask">Entered tasks will be shown here</h3>`;
-}
